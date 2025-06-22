a26d56aa20dd86e6c19f4bde6c99ab16bf5d8e75
jiaoyuyang <jiaoyuyang@live.com>
2025-06-22 21:49:30 +0800

    bind mq

--- src/main/java/com/pingan/dispatcherservice/DispatcherServiceApplication.java
+++ src/main/java/com/pingan/dispatcherservice/DispatcherServiceApplication.java
@@ -3,11 +3,14 @@ package com.pingan.dispatcherservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
 @SpringBootApplication
 public class DispatcherServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DispatcherServiceApplication.class, args);
-	}
+
+    }
 
 }
